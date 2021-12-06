a14eb5fbf06fd2c150884ce0072c0f11c156c251
mnecas <necas.marty@gmail.com>
2021-12-06 13:45:08 +0100

    add skip for import

--- automation/build.sh
+++ automation/build.sh
@@ -70,7 +70,7 @@ cd $COLLECTION_DIR
 
 pip3 install rstcheck antsibull-changelog "ansible-lint<5.0.0"
 
-ansible-test sanity
+ansible-test sanity --skip-test import
 antsibull-changelog lint
 ansible-lint roles/* -x 204
 
